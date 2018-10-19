5769d71e4e97fe7e512fda2df8d12c9326ed314c
imanku <43012896+imanku@users.noreply.github.com>
2018-10-19 17:59:41 -0400

    week6lab

--- week-6-lab.cpp
+++ week-6-lab.cpp
@@ -4,19 +4,31 @@ int main()
 {
 
  int X;
-    cout<<"Input Your Score :   ";
+    cout<<"Input Your Score :   "; //user input a score 
+
     cin>>X;
  if (X>=86)
-    cout<< "Grade Is A ";
-   else if (X>=72)
-    cout<< "Grade Is B ";
-   else if (X>=60)
-    cout<< "Grade Is C";
-   else if (X>=50)
-    cout<< "Grade Is D";
-   else if (X<50)
-    cout<< "Grade Is F";
+
+ {
+ cout<< "Grade Is A ";//logic flow determining which grade is proper for the score
+ }  
+ else if (X>=72)
+ { 
+   cout<< "Grade Is B ";
+ }
+  else if (X>=60)
+ {
+  cout<< "Grade Is C";
+ }
+  else if (X>=50)
+ {
+  cout<< "Grade Is D";
+ }
+  else
+ {
+  cout<< "Grade Is F";//the grade will come F if the score does not match any of the if else statements
+ }
     
-    return 0;
+    return 0; //return the 0 value in return
 
 }
